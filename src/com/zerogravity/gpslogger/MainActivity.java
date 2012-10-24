4171f60beb61b0ed6622ef53841cdce2a5c61a74
Ashwin Ramesh Sidhalinganahalli <rsashwin@gmail.com>
2012-10-24 00:06:54 -0400

    changes

--- src/com/zerogravity/gpslogger/MainActivity.java
+++ src/com/zerogravity/gpslogger/MainActivity.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -20,8 +22,10 @@ public class MainActivity extends Activity {
     PendingIntent sender;
     GPSTracker gps;
     TextView tv;
-    
-    
+
+    LocationListener mlocListener;
+    LocationManager mlocManager;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
@@ -29,22 +33,21 @@ public class MainActivity extends Activity {
 
 	TurnOff = (Button) findViewById(R.id.btOff);
 	LogLocationNow = (Button) findViewById(R.id.bRib);
-	tv = (TextView)findViewById(R.id.t1);
-	    gps = new GPSTracker(getApplicationContext());
+	tv = (TextView) findViewById(R.id.t1);
+	gps = new GPSTracker(getApplicationContext());
 
-	    // check if GPS enabled
-	    if (gps.canGetLocation()){
-		
-		//logLocationToFile();
-		
-	    }else{
-		// can't get location
-		// GPS or Network is not enabled
-		// Ask user to enable GPS/network in settings
-		gps.showSettingsAlert();
-	    }
-	    
-	    
+	// check if GPS enabled
+	if (gps.canGetLocation()) {
+
+	    // logLocationToFile();
+
+	} else {
+	    // can't get location
+	    // GPS or Network is not enabled
+	    // Ask user to enable GPS/network in settings
+	    gps.showSettingsAlert();
+	}
+
 	LogLocationNow.setOnClickListener(new OnClickListener() {
 	    @Override
 	    public void onClick(View v) {
@@ -65,14 +68,15 @@ public class MainActivity extends Activity {
 	});
     }
 
-    public void PlayWithRawFiles() {      
-	
+    public void PlayWithRawFiles() {
+
 	File sdcard = Environment.getExternalStorageDirectory();
 
-	//Get the text file
-	File file = new File(sdcard,File.separator+Constants.DIRECTORY_NAME+File.separator+Constants.FILE_NAME);
+	// Get the text file
+	File file = new File(sdcard, File.separator + Constants.DIRECTORY_NAME
+		+ File.separator + Constants.FILE_NAME);
 
-	//Read text from file
+	// Read text from file
 	StringBuilder text = new StringBuilder();
 
 	try {
@@ -80,20 +84,18 @@ public class MainActivity extends Activity {
 	    String line;
 
 	    while ((line = br.readLine()) != null) {
-	        text.append(line);
-	        text.append('\n');
+		text.append(line);
+		text.append('\n');
 	    }
-	}
-	catch (IOException e) {
-	    //You'll need to add proper error handling here
+	} catch (IOException e) {
+	    // You'll need to add proper error handling here
 	}
 
-	//Find the view by its id
-	TextView tv = (TextView)findViewById(R.id.t1);
+	// Find the view by its id
+	TextView tv = (TextView) findViewById(R.id.t1);
 
-	//Set the text
-	tv.setText(text);		
+	// Set the text
+	tv.setText(text);
     }
 
 }
-
--- src/com/zerogravity/gpslogger/MyLocationListener.java
+++ src/com/zerogravity/gpslogger/MyLocationListener.java
@@ -9,7 +9,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.widget.Toast;
+import android.util.Log;
 
 /**
  * @author Ash
@@ -17,95 +17,34 @@ import android.widget.Toast;
  */
 public class MyLocationListener extends Service implements LocationListener {
 
-    float[] resultss = new float[2];
+  
 
     @Override
     public void onLocationChanged(Location loc) {
 	// TODO Auto-generated method stub
-	if (MainActivity.gps_state == Constants.GPS_State.GPS_Not_Acquired) {
+	Log.i("MLL","onLocationChanged...");
 
-	    getStartingLocation(loc);
-
-	} else {
-	    /*
-	     * GPS is acquired and waiting for more accurate position
-	     */
-	    if (MainActivity.counter_state == Counter_State.Counter_ON) {
-		if (loc.getAccuracy() < Constants.accuracy_threshold) {
-
-		    // Update the moving coordinates
-		    MainActivity.stop_longitude = loc.getLongitude();
-		    MainActivity.stop_latitude = loc.getLatitude();
-		    Location.distanceBetween(MainActivity.start_latitude,
-			    MainActivity.start_longitude,
-			    MainActivity.stop_latitude,
-			    MainActivity.stop_longitude, resultss);
-
-		    if (resultss[0] >= 50) {
-			MainActivity.gps_TV.setText("Reached 50Mts...!");
-			String Text = "You reached 50mts!" + "Accuracy-"
-				+ loc.getAccuracy() + "Distance-" + resultss[0];
-			Toast.makeText(MainActivity.getAppContext(), Text,
-				Toast.LENGTH_LONG).show();
-			MainActivity.stopCounter();
-		    } else {
-			MainActivity.gps_TV.setText("Accuracy-"
-				+ loc.getAccuracy() + " Distance-"
-				+ resultss[0]);
-		    }
-
-		} else {
-		    MainActivity.gps_TV.setText("Accuracy-" + loc.getAccuracy()
-			    + " Distance-" + resultss[0]);
-		}
-	    } else {
-		// Update the starting point until the counter is pressed
-		getStartingLocation(loc);
-	    }
-
-	}
 
     }
 
-    public void getStartingLocation(Location loc) {
-	/*
-	 * GPS is acquired and waiting for more accurate position
-	 */
-	if (loc.getAccuracy() < Constants.ACCURACY_THRESHOLD) {
-	    MainActivity.gps_TV.setText("GPS Acquired...READY!");
-	    MainActivity.start_longitude = loc.getLongitude();
-	    MainActivity.start_latitude = loc.getLatitude();
-	    MainActivity.gps_state = Constants.GPS_State.GPS_Acquired;
-	} else {
-	    if (MainActivity.gps_state == Constants.GPS_State.GPS_Acquired) {
-		MainActivity.gps_TV.setText("GPS Acquired..READY!");
-	    } else {
-		MainActivity.gps_TV.setText("Acquiring GPS...");
-	    }
-	}
-    }
 
     @Override
     public void onProviderDisabled(String provider) {
 	// TODO Auto-generated method stub
-	MainActivity.gps_TV.setText("Provider Disabled...");
-	Toast.makeText(MainActivity.getAppContext(), "Gps Disabled",
-		Toast.LENGTH_SHORT).show();
-
+	Log.i("MLL","Provider Disabled...");
     }
 
     @Override
     public void onProviderEnabled(String provider) {
 	// TODO Auto-generated method stub
-	MainActivity.gps_TV.setText("Provider Enabled...");
-	Toast.makeText(MainActivity.getAppContext(), "Gps Enabled",
-		Toast.LENGTH_SHORT).show();
+	Log.i("MLL","Provider Enabled...");
+
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
 	// TODO Auto-generated method stub
-	MainActivity.gps_TV.setText("Status Changed..." + status);
+	Log.i("MLL","onStatusChanged...");
     }
 
     @Override
